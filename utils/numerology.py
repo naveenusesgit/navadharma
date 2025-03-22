e513695212746a5846120d06b00665df1db84aae
naveenusesgit <varadarajan.naveen@gmail.com>
2025-03-23 02:14:18 +0530

    Update numerology.py

--- utils/numerology.py
+++ utils/numerology.py
@@ -1,24 +1,30 @@
+# utils/numerology.py
+
 def reduce_to_single_digit(n):
-    while n > 9 and n not in [11, 22, 33]:  # Master numbers
+    while n > 9 and n not in [11, 22, 33]:  # master numbers
         n = sum(int(d) for d in str(n))
     return n
 
-def get_life_path_number(dob: str):
-    parts = [int(p) for p in dob.split("-")]  # YYYY-MM-DD
-    total = sum(int(d) for d in str(parts[0]) + str(parts[1]) + str(parts[2]))
+def get_life_path(date_str):
+    parts = date_str.split("-")
+    digits = "".join(parts)
+    return reduce_to_single_digit(sum(int(d) for d in digits))
+
+def get_destiny_number(name):
+    pythagorean = {
+        "A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8,"I":9,
+        "J":1,"K":2,"L":3,"M":4,"N":5,"O":6,"P":7,"Q":8,"R":9,
+        "S":1,"T":2,"U":3,"V":4,"W":5,"X":6,"Y":7,"Z":8
+    }
+    name = name.upper().replace(" ", "")
+    total = sum(pythagorean.get(char, 0) for char in name)
     return reduce_to_single_digit(total)
 
-def get_name_number(name):
-    name = name.upper().replace(" ", "")
-    numerology_map = {
-        'A':1,'B':2,'C':3,'D':4,'E':5,'F':8,'G':3,'H':5,'I':1,
-        'J':1,'K':2,'L':3,'M':4,'N':5,'O':7,'P':8,'Q':1,'R':2,
-        'S':3,'T':4,'U':6,'V':6,'W':6,'X':5,'Y':1,'Z':7
-    }
-    return reduce_to_single_digit(sum(numerology_map.get(char, 0) for char in name))
-
-def get_numerology_summary(name, dob):
+def numerology_profile(name, dob):
+    life_path = get_life_path(dob)
+    destiny = get_destiny_number(name)
     return {
-        "life_path": get_life_path_number(dob),
-        "destiny": get_name_number(name),
+        "lifePathNumber": life_path,
+        "destinyNumber": destiny,
+        "luckyNumbers": [life_path, destiny, life_path + destiny]
     }
