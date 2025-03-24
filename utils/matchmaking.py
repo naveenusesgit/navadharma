9ac85991be7af4470f8d548546febdfcae73bf30
naveenusesgit <varadarajan.naveen@gmail.com>
2025-03-24 21:02:11 +0530

    Update matchmaking.py

--- utils/matchmaking.py
+++ utils/matchmaking.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from match import run_matchmaking
+from utils_root.match import run_matchmaking
 from pydantic import BaseModel
 
 
