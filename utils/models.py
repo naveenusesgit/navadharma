ad39108bccd39bccf2da9617db8a4e406b976b0f
naveenusesgit <varadarajan.naveen@gmail.com>
2025-03-26 13:37:31 +0530

    Create models.py

---
+++ utils/models.py
@@ -0,0 +1,22 @@
+# utils/models.py
+
+from pydantic import BaseModel
+
+class KundliRequest(BaseModel):
+    datetime: str
+    place: str
+    latitude: float
+    longitude: float
+    timezone: float
+
+class MonthlyRequest(BaseModel):
+    rashi: str
+    month: str = None
+
+class WeeklyRequest(BaseModel):
+    rashi: str
+    week: str = None
+
+class NumerologyRequest(BaseModel):
+    name: str
+    dob: str  # Format: YYYY-MM-DD
