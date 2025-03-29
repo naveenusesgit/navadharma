83bd0ab14bde57c3d3de8708e87939d4a21ec777
naveenusesgit <varadarajan.naveen@gmail.com>
2025-03-29 14:03:57 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,17 +1,9 @@
-from .kundli import (
-    generate_kundli_chart,
-    get_lagna_info,
-)
-
-from .monthly_prediction import get_monthly_prediction
-from .weekly_prediction import get_weekly_prediction
-from .numerology import get_numerology
-from .interpretations import get_yogas
-from .panchanga_calendar import generate_panchanga_calendar
-from .muhurat_finder import find_muhurats
-from .pdf_utils import generate_kundli_report_pdf
-
-# ✅ NOTE: generate_full_kundli_prediction comes from a separate module
-# So it should NOT be imported from kundli.py
-# Use this in main.py directly:
-# from utils.full_kundli_prediction import generate_full_kundli_prediction
+def generate_kundli_chart(name, date, time, place):
+    # Logic...
+    return {
+        "name": name,
+        "date": date,
+        "time": time,
+        "place": place,
+        "message": "Kundli chart logic would be implemented here."
+    }
